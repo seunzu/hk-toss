26892d67b06fd7894123835d7097d58168ab3285
zzoo0123 <0123suh@gmail.com>
2024-10-14 18:48:34 +0900

    StoreRepository fix

--- spring/spring-db/src/main/java/com/example/springdb/repository/StoreRepository.java
+++ spring/spring-db/src/main/java/com/example/springdb/repository/StoreRepository.java
@@ -15,18 +15,18 @@ public interface StoreRepository extends JpaRepository<Store, Long> {
     @Query("SELECT s FROM Store s WHERE s.name LIKE %?1%")
     List<Store> findByNameContain(String name);
 
-//    @Transactional
-//    @Modifying
-    // 1. 이름으로 삭제
-//    @Query("DELETE FROM Store s WHERE s.name = ?1")
-//    void deleteByName(String name);
+//     1. 이름으로 삭제
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM Store s WHERE s.name = :name")
+    void deleteByName(String name);
 
-    // 2. 아이디가 5 이상의 것들 출력
-//    @Query("SELECT s FROM Store s WHERE s.id >= 5")
-//    List<Store> findByNameLike(String name);
+//     2. 아이디가 5 이상의 것들 출력
+    @Query("SELECT s FROM Store s WHERE s.id >= 5")
+    List<Store> findByNameLike();
 
-    // 3. name address 둘 중 하나라도 들어가있는 것 출력
-//    @Query("SELECT s FROM Store s WHERE s.name LIKE %?1% OR s.address LIKE %?1%")
-//    List<Store> findByNameOrAddressContaining(String keyword);
+//     3. '매'가 포함된 name address 둘 중 하나라도 들어가있는 것 출력
+    @Query("SELECT s FROM Store s WHERE s.name LIKE %:keyword% OR s.address LIKE %:keyword%")
+    List<Store> findByNameOrAddressContaining(String keyword);
 
 }
