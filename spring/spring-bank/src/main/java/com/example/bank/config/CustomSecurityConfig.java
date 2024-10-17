5dee5920ba9220b52a88fbe5e87c3dec7cb52b11
zzoo123 <0123suh@gmail.com>
2024-10-17 16:51:41 +0900

    fix: 401 error

--- spring/spring-bank/src/main/java/com/example/bank/config/CustomSecurityConfig.java
+++ spring/spring-bank/src/main/java/com/example/bank/config/CustomSecurityConfig.java
@@ -5,13 +5,14 @@ import com.example.bank.global.JwtUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-@Configurable
+@Configuration
 @RequiredArgsConstructor
 public class CustomSecurityConfig {
 
